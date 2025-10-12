a879b62f29e0db26c1ac8e18e8f3824f032060e6
Angel Dominguez Plata <angeldomp49@gmail.com>
2025-10-12 16:23:40 -0600

    feat(Environment): Environment development configuration

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -1,11 +1,12 @@
 import { PassedInitialConfig } from 'angular-auth-oidc-client';
+import {environment} from '../../environments/environment';
 
 export const authConfig: PassedInitialConfig = {
   config: {
-            authority: 'https://cognito-idp.us-east-2.amazonaws.com/us-east-2_kZeUTI40G',
-            redirectUrl: "https://d280b0wq9js4xf.cloudfront.net/after-login",
+            authority: `${environment.cognitoAuthorityUrl}/${environment.cognitoUsersPoolId}`,
+            redirectUrl: `${environment.remoteHostname}/after-login`,
             postLogoutRedirectUri: window.location.origin,
-            clientId: '1t71utkcgnhjmjm7c0fr6f54po',
+            clientId: environment.clientId,
             usePushedAuthorisationRequests: false,
             scope: 'email openid profile',
             responseType: 'code',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,9 @@ export const environment = {
   isProduction: true,
   isAuthenticationEnabled: true,
   homeEndpoint: "/keyring",
-  loginEndpoint: "/login-check"
+  loginEndpoint: "/login-check",
+  clientId: '1t71utkcgnhjmjm7c0fr6f54po',
+  remoteHostname: 'https://d280b0wq9js4xf.cloudfront.net',
+  cognitoAuthorityUrl: 'https://cognito-idp.us-east-2.amazonaws.com',
+  cognitoUsersPoolId: 'us-east-2_kZeUTI40G'
 };
