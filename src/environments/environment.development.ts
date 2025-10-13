d9aa9b049ce194a854a75266ea8356f250666d58
Angel Dominguez Plata <angeldomp49@gmail.com>
2025-10-12 18:47:44 -0600

    feat(Environment): Environment development configuration

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -5,7 +5,7 @@ export const environment = {
   homeEndpoint: "/keyring",
   loginEndpoint: "/login-check",
   cognitoClientId: '1t71utkcgnhjmjm7c0fr6f54po',
-  remoteHostname: 'https://develop.d1u57utxeih152.amplifyapp.com',
+  remoteHostname: 'https://dxr6cwch5bltx.cloudfront.net',
   cognitoAuthorityUrl: 'https://cognito-idp.us-east-2.amazonaws.com',
   cognitoUsersPoolId: 'us-east-2_kZeUTI40G'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
   homeEndpoint: "/keyring",
   loginEndpoint: "/login-check",
   cognitoClientId: '1t71utkcgnhjmjm7c0fr6f54po',
-  remoteHostname: 'https://d280b0wq9js4xf.cloudfront.net',
+  remoteHostname: 'https://dxr6cwch5bltx.cloudfront.net',
   cognitoAuthorityUrl: 'https://cognito-idp.us-east-2.amazonaws.com',
   cognitoUsersPoolId: 'us-east-2_kZeUTI40G'
 };
