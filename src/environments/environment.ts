b7b4103752017531c2b92d40ee88281977b04427
Angel Dominguez Plata <angeldomp49@gmail.com>
2025-10-17 21:04:12 -0600

    fix(Environment): Update default configuration to production and change remote hostname

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
   homeEndpoint: "/keyring",
   loginEndpoint: "/login-check",
   cognitoClientId: '1t71utkcgnhjmjm7c0fr6f54po',
-  remoteHostname: 'https://d3udo965unl57n.cloudfront.net',
+  remoteHostname: 'https://d280b0wq9js4xf.cloudfront.net',
   cognitoAuthorityUrl: 'https://cognito-idp.us-east-2.amazonaws.com',
   cognitoUsersPoolId: 'us-east-2_kZeUTI40G'
 };
